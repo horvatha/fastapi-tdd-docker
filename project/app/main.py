7bc7a30054fb5e3e441d38186b179b3a660b545b
Árpád Horváth <horvath.arpad.szfvar@gmail.com>
2020-09-21 19:45:05 +0200

    Refactoring the app using the fastapi.APIRouter

--- project/app/main.py
+++ project/app/main.py
@@ -1,25 +1,24 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 import os
 from tortoise.contrib.fastapi import register_tortoise
 
 
-from app.config import get_settings, Settings
-
-app = FastAPI()
-
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
+from app.api import ping
 
 
-@app.get("/ping")
-def pong(settings: Settings = Depends(get_settings)):
-    return dict(
-        ping="pong!",
-        environment=settings.environment,
-        testing=settings.testing
+def create_application():
+    application = FastAPI()
+
+    register_tortoise(
+        application,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
     )
+
+    application.include_router(ping.router)
+    return application
+
+
+app = create_application()
