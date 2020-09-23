d42d4e7c8e97ff197528f0bc2e374f29190dc988
Árpád Horváth <horvath.arpad.szfvar@gmail.com>
2020-09-23 16:21:57 +0200

    RESTful Routes POST & GET TextSummary (Part 1, Chapter 8, not the end)

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,7 +1,5 @@
-# project/app/models/tortoise.py
-
-
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class TextSummary(models.Model):
@@ -12,3 +10,5 @@ class TextSummary(models.Model):
     def __str__(self):
         return self.url
 
+
+SummarySchema = pydantic_model_creator(TextSummary)
